d24bdb955aeddb88f7b9aa3d9695aceec77a606b
Jareth Gomes <jarethgomes@gmail.com>
2023-12-11 01:46:10 +0000

    Added test module to daily template part 1

--- daily-template/src/part1.rs
+++ daily-template/src/part1.rs
@@ -1,3 +1,15 @@
 pub fn part1(input: &str) -> &str {
     unimplemented!()
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = "";
+
+        assert_eq!(part1(input), "");
+    }
+}
